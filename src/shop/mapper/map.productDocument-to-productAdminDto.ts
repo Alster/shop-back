c6581da202a30cedfa8d22df997dfe8f07c84b8e
АндрейКо <anik123wb@gmail.com>
2023-04-17 15:50:39 +0300

    product localization

---
+++ src/shop/mapper/map.productDocument-to-productAdminDto.ts
@@ -0,0 +1,19 @@
+import { ProductAdminDto, ProductDto } from '@shop/shared/dto/product.dto';
+import { ProductDocument } from '../schema/product.schema';
+
+export function mapProductDocumentToProductAdminDto(
+  obj: ProductDocument,
+): ProductAdminDto {
+  return {
+    id: obj._id.toString(),
+    title: obj.title,
+    description: obj.description,
+    categories: obj.categories.map((category) => category.toString()),
+    items: obj.items,
+    attrs: obj.attrs,
+    quantity: obj.quantity,
+    price: obj.price,
+    active: obj.active,
+    createDate: 'no any date ololo',
+  };
+}
